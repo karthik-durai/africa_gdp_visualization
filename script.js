59747d21b82eb6a5cd361a03e205c9f6cf6616e3
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2019-09-29 21:17:30 +0530

    create color codes

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ L.tileLayer(`https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=${t
   accessToken: thunderStormToken
 }).addTo(mymap);
 
+const colorCodes = {
+  5: '#e6194B',
+  10: '#f58231',
+  20: '#ffe119',
+  50: '#bfef45',
+  100: '#aaffc3',
+  200: '#3cb44b'
+}
